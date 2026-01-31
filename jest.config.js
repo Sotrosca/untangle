853c1ebc938ee018bf227c24e1f7d7987e9edb39
Facundo Serna <facuserna93@gmail.com>
2026-01-31 18:57:25 -0300

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "jest-expo",
+  testMatch: ["**/tests/**/*.test.(ts|tsx)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+};
